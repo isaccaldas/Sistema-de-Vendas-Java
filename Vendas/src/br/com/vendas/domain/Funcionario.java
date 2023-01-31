12f39060793690b03f77984bbf216debfc12d02d
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2023-01-31 08:19:22 -0300

    criando as classe Funcionario, Produto e outras configurações.

---
+++ Vendas/src/br/com/vendas/domain/Funcionario.java
@@ -0,0 +1,74 @@
+package br.com.vendas.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_funcionarios") // faz o mapeamento com o nome da tabela
+public class Funcionario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO) // auto increment
+	@Column(name="fun_codigo")
+	private Long codigo; // Long por ser um bigint
+	
+	@Column(name="fun_nome", length = 50, nullable = false)
+	private String nome;
+	
+	@Column(name="fun_cpf", length = 14, nullable = false, unique = true)
+	private String cpf;
+	
+	@Column(name="fun_senha", length = 50, nullable = false)
+	private String senha;
+	
+	@Column(name="fun_funcao", length = 50, nullable = false)
+	private String funcao;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFuncao() {
+		return funcao;
+	}
+
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+	
+	
+	
+	
+}
